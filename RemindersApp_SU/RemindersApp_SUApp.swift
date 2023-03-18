da9a5f8916d4ce77fbdd2d5acc33336642b7d439
Nadia <ivashchenko.nadia@gmail.com>
2023-03-18 17:17:13 +0200

    Initial Commit

---
+++ RemindersApp_SU/RemindersApp_SUApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindersApp_SUApp.swift
+//  RemindersApp_SU
+//
+//  Created by Students on 18.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct RemindersApp_SUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
